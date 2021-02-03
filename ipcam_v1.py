dbad8b96147eb31405356a0e25d60ed10c67eacd
Cbenitesdev <78431820+Cbenitesdev@users.noreply.github.com>
2021-02-03 10:38:19 -0500

    Create ipcam_v1.py

---
+++ ipcam_v1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import time
+import cv2
+
+cap = cv2.VideoCapture('rtsp://admin:admin@192.168.1.73/cam1/mpeg4')
+#cap = cv2.VideoCapture(0)
+
+while True:
+
+    #print('About to start the Read command')
+    ret, frame = cap.read()
+    #print('About to show frame of Video.')
+    cv2.imshow("Capturing",frame)
+    #print('Running..')
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
